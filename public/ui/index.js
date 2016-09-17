eb90f6a8a43b254da3e7cc81a336fb7dacc070b4
Anna Frederich <arfred@mit.edu>
2016-09-17 16:03:07 -0400

    add in more stuff

--- public/ui/index.js
+++ public/ui/index.js
@@ -1,9 +1,34 @@
 
+var UserProfile = React.createClass({
+  render: function() {
+    return (
+      <div className="userProfile">
+        <div>Name</div>
+
+      </div>
+    );
+  }
+});
+
+var Timeline = React.createClass({
+  render: function() {
+    return (
+      <div className="timeline">
+        <div>Event 1</div>
+        <div>Event 2</div>
+        <div>Event 3</div>
+        <div>Event 4</div>
+      </div>
+    );
+  }
+});
+
 var App = React.createClass({
   render: function() {
     return (
-      <div style={style} className="container">
-        Anna
+      <div className="container">
+        <UserProfile />
+        <Timeline />
       </div>
     );
   }
---
+++ public/ui/timeline.js
@@ -0,0 +1,14 @@
+var timeline = React.createClass({
+  render: function() {
+    return (
+      <div style={style} className="timeline">
+        <div>Event 1</div>
+        <div>Event 2</div>
+        <div>Event 3</div>
+        <div>Event 4</div>
+      </div>
+    );
+  }
+});
+
+module.exports = Timeline
